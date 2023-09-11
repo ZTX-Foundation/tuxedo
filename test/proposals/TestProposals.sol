ce5c4fd25397279cca0f5786360540b23b8abc94
LUKE™ <utx0@mettadata.com>
2023-09-11 21:28:17 +1000

    fix: syntax styling

--- test/proposals/TestProposals.sol
+++ test/proposals/TestProposals.sol
@@ -7,6 +7,7 @@ import {Addresses} from "@test/proposals/Addresses.sol";
 import {Proposal} from "@test/proposals/proposalTypes/Proposal.sol";
 
 import {zip000} from "@test/proposals/zips/zip000.sol";
+import {zip001} from "@test/proposals/zips/zip001.sol";
 
 /*
 How to use:
@@ -24,37 +25,40 @@ contract TestProposals is Test {
     Addresses public addresses;
     Proposal[] public proposals;
     uint256 public nProposals;
-    bool public DEBUG;
-    bool public DO_DEPLOY;
-    bool public DO_AFTER_DEPLOY;
-    bool public DO_BUILD;
-    bool public DO_RUN;
-    bool public DO_TEARDOWN;
-    bool public DO_VALIDATE;
+    bool public debug;
+    bool public doDeploy;
+    bool public doAfterDeploy;
+    bool public doBuild;
+    bool public doRun;
+    bool public doTeardown;
+    bool public doValidate;
 
     function setUp() public {
-        DEBUG = vm.envOr("DEBUG", true);
-        DO_DEPLOY = vm.envOr("DO_DEPLOY", true);
-        DO_AFTER_DEPLOY = vm.envOr("DO_AFTER_DEPLOY", true);
-        DO_BUILD = vm.envOr("DO_BUILD", true);
-        DO_RUN = vm.envOr("DO_RUN", true);
-        DO_TEARDOWN = vm.envOr("DO_TEARDOWN", true);
-        DO_VALIDATE = vm.envOr("DO_VALIDATE", true);
+        debug = vm.envOr("DEBUG", true);
+        doDeploy = vm.envOr("DO_DEPLOY", true);
+        doAfterDeploy = vm.envOr("DO_AFTER_DEPLOY", true);
+        doBuild = vm.envOr("DO_BUILD", true);
+        doRun = vm.envOr("DO_RUN", true);
+        doTeardown = vm.envOr("DO_TEARDOWN", true);
+        doValidate = vm.envOr("DO_VALIDATE", true);
         addresses = new Addresses();
 
+        // Load proposals
         proposals.push(Proposal(address(new zip000())));
+        proposals.push(Proposal(address(new zip001())));
+
         nProposals = proposals.length;
     }
 
     function setDebug(bool value) public {
-        DEBUG = value;
+        debug = value;
         for (uint256 i = 0; i < proposals.length; i++) {
             proposals[i].setDebug(value);
         }
     }
 
     function testProposals() public returns (uint256[] memory postProposalVmSnapshots) {
-        if (DEBUG) {
+        if (debug) {
             console.log("TestProposals: running", proposals.length, "proposals.");
         }
         postProposalVmSnapshots = new uint256[](proposals.length);
@@ -62,13 +66,13 @@ contract TestProposals is Test {
             string memory name = proposals[i].name();
 
             // Deploy step
-            if (DO_DEPLOY) {
-                if (DEBUG) {
+            if (doDeploy) {
+                if (debug) {
                     console.log("Proposal", name, "deploy()");
                     addresses.resetRecordingAddresses();
                 }
                 proposals[i].deploy(addresses, address(this));
-                if (DEBUG) {
+                if (debug) {
                     (string[] memory recordedNames, address[] memory recordedAddresses) = addresses
                         .getRecordedAddresses();
                     for (uint256 j = 0; j < recordedNames.length; j++) {
@@ -78,36 +82,36 @@ contract TestProposals is Test {
             }
 
             // After-deploy step
-            if (DO_AFTER_DEPLOY) {
-                if (DEBUG) console.log("Proposal", name, "afterDeploy()");
+            if (doAfterDeploy) {
+                if (debug) console.log("Proposal", name, "afterDeploy()");
                 proposals[i].afterDeploy(addresses, address(proposals[i]));
             }
 
             // Build step
-            if (DO_BUILD) {
-                if (DEBUG) console.log("Proposal", name, "build()");
+            if (doBuild) {
+                if (debug) console.log("Proposal", name, "build()");
                 proposals[i].build(addresses);
             }
 
             // Run step
-            if (DO_RUN) {
-                if (DEBUG) console.log("Proposal", name, "run()");
+            if (doRun) {
+                if (debug) console.log("Proposal", name, "run()");
                 proposals[i].run(addresses, address(proposals[i]));
             }
 
             // Teardown step
-            if (DO_TEARDOWN) {
-                if (DEBUG) console.log("Proposal", name, "teardown()");
+            if (doTeardown) {
+                if (debug) console.log("Proposal", name, "teardown()");
                 proposals[i].teardown(addresses, address(proposals[i]));
             }
 
             // Validate step
-            if (DO_VALIDATE) {
-                if (DEBUG) console.log("Proposal", name, "validate()");
+            if (doValidate) {
+                if (debug) console.log("Proposal", name, "validate()");
                 proposals[i].validate(addresses, address(proposals[i]));
             }
 
-            if (DEBUG) console.log("Proposal", name, "done.");
+            if (debug) console.log("Proposal", name, "done.");
 
             postProposalVmSnapshots[i] = vm.snapshot();
         }
