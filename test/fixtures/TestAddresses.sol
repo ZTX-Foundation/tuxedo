84ecd828b7af5313cee97fd3f0e5596461efe8b5
LUKE™ <utx0@mettadata.com>
2023-09-22 15:10:50 +1000

    fix: addresses updated to avoid clashes

--- test/fixtures/TestAddresses.sol
+++ test/fixtures/TestAddresses.sol
@@ -2,19 +2,19 @@
 pragma solidity 0.8.18;
 
 library TestAddresses {
-    address public constant userAddress = address(0x1);
-    address public constant secondUserAddress = address(0x2);
-    address public constant beneficiaryAddress1 = address(0x3);
-    address public constant beneficiaryAddress2 = address(0x4);
-    address public constant tokenGovernorAddress = address(0x5);
-    address public constant financialControllerAddress = address(0x6);
-    address public constant minterAddress = address(0x7);
-    address public constant guardianAddress = address(0x8);
-    address public constant financialGuardianAddress = address(0x9);
-    address public constant deployerAddress = address(0x10);
-    address public constant adminAddress = address(0x11);
-    address public constant safeAddress = address(0x12);
-    address public constant minterNotaryAddress = address(0x13);
-    address public constant lockerAddress = address(0x14);
-    address public constant registryOperatorAddress = address(0x15);
+    address public constant userAddress = address(0x001);
+    address public constant secondUserAddress = address(0x002);
+    address public constant beneficiaryAddress1 = address(0x003);
+    address public constant beneficiaryAddress2 = address(0x004);
+    address public constant tokenGovernorAddress = address(0x005);
+    address public constant financialControllerAddress = address(0x006);
+    address public constant minterAddress = address(0x007);
+    address public constant guardianAddress = address(0x008);
+    address public constant financialGuardianAddress = address(0x009);
+    address public constant deployerAddress = address(0x010);
+    address public constant adminAddress = address(0x011);
+    address public constant safeAddress = address(0x012);
+    address public constant minterNotaryAddress = address(0x013);
+    address public constant lockerAddress = address(0x014);
+    address public constant registryOperatorAddress = address(0x015);
 }
