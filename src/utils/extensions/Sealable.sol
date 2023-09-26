1b6377fc5ff12de925c64a40895b5e7fb1002142
LUKE™ <utx0@mettadata.com>
2023-09-26 14:21:40 +1000

    chore: added natspec as requested

--- src/utils/extensions/Sealable.sol
+++ src/utils/extensions/Sealable.sol
@@ -1,5 +1,7 @@
 pragma solidity 0.8.18;
 
+/// @notice This sealable contract extenions can be used to create a single use function that is only callable once and then locked.
+/// @dev note that this can only be used on a single function call per contract due to the single seal variable.
 abstract contract Sealable {
     bool public seal = false;
 
