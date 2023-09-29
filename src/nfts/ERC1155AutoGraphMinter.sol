293b1f2a6446473bcffcd6a5856cbb4694929321
_IM <70502803+intl-man@users.noreply.github.com>
2023-09-29 08:42:00 +0800

    chore: added missing license identifier (#28)

--- src/nfts/ERC1155AutoGraphMinter.sol
+++ src/nfts/ERC1155AutoGraphMinter.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNKNOWN
+// SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.8.18;
 
 import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
