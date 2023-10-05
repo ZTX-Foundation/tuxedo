fae17c657b842b44e95c99a4199f2b70973a06f6
LUKE™ <utx0@mettadata.com>
2023-10-06 08:14:44 +1100

    wip - started zip001

--- proposals/zips/zip001.sol
+++ proposals/zips/zip001.sol
@@ -11,6 +11,10 @@ import {Core} from "@protocol/core/Core.sol";
 import {Roles} from "@protocol/core/Roles.sol";
 import {Token} from "@protocol/token/Token.sol";
 import {ERC20HoldingDeposit} from "@protocol/finance/ERC20HoldingDeposit.sol";
+import {ERC1155MaxSupplyMintable} from "@protocol/nfts/ERC1155MaxSupplyMintable.sol";
+import {ERC1155AutoGraphMinter} from "@protocol/nfts/ERC1155AutoGraphMinter.sol";
+import {GlobalReentrancyLock} from "@protocol/core/GlobalReentrancyLock.sol";
+import {GameConsumer} from "@protocol/game/GameConsumer.sol";
 
 contract zip001 is Proposal, TimelockProposal {
     string public name = "ZIP001";
@@ -24,7 +28,70 @@ contract zip001 is Proposal, TimelockProposal {
         assertEq(_core.hasRole(Roles.ADMIN, deployer), true);
     }
 
-    function _deploy(Addresses addresses, address) internal override {}
+    function _deploy(Addresses addresses, address) internal override {
+        /// GlobalReentrancyLock
+        GlobalReentrancyLock globalReentrancyLock = new GlobalReentrancyLock(addresses.getAddress("CORE"));
+        addresses.addAddress("GLOBAL_REENTRANCY_LOCK", address(globalReentrancyLock));
+
+        /// NTF contracts
+        /// Setup metadata base uri
+        string memory _metadataBaseUri = string(
+            abi.encodePacked("https://meta.", vm.envString("ENVIRONMENT"), ".", vm.envString("DOMAIN"), "/")
+        );
+
+        /// Consumables NFT contract
+        ERC1155MaxSupplyMintable erc1155Consumables = new ERC1155MaxSupplyMintable(
+            address(_core),
+            string(abi.encodePacked(_metadataBaseUri, "consumables/metadata/")),
+            "ZTX Consumables",
+            "ZTXC"
+        );
+        addresses.addAddress("ERC1155_MAX_SUPPLY_MINTABLE_CONSUMABLES", address(erc1155Consumables));
+
+        /// Placeables NFT contract
+        ERC1155MaxSupplyMintable erc1155Placeables = new ERC1155MaxSupplyMintable(
+            address(_core),
+            string(abi.encodePacked(_metadataBaseUri, "placeables/metadata/")),
+            "ZTX Placeables",
+            "ZTXP"
+        );
+        addresses.addAddress("ERC1155_MAX_SUPPLY_MINTABLE_PLACEABLES", address(erc1155Placeables));
+
+        /// Wearables NFT contract
+        ERC1155MaxSupplyMintable erc1155Wearables = new ERC1155MaxSupplyMintable(
+            address(_core),
+            string(abi.encodePacked(_metadataBaseUri, "wearables/metadata/")),
+            "ZTX Wearables",
+            "ZTXW"
+        );
+        addresses.addAddress("ERC1155_MAX_SUPPLY_MINTABLE_WEARABLES", address(erc1155Wearables));
+
+        /// AutoGraphMinter contract
+        address[] memory nftContractAddresses = new address[](3);
+        nftContractAddresses[0] = address(erc1155Consumables);
+        nftContractAddresses[1] = address(erc1155Placeables);
+        nftContractAddresses[2] = address(erc1155Wearables);
+
+        ERC1155AutoGraphMinter erc1155AutoGraphMinter = new ERC1155AutoGraphMinter(
+            address(_core),
+            nftContractAddresses,
+            10_000e18, // TODO get offical values from the HQ
+            10_000_000e18,
+            addresses.getAddress("AUTOGRAPH_MINTER_PAYMENT_RECIPIENT"),
+            1 // 1 hour for valid expiryToken
+        );
+        addresses.addAddress("ERC1155_AUTO_GRAPH_MINTER", address(erc1155AutoGraphMinter));
+
+        /// Game consumer
+        GameConsumer consumer = new GameConsumer(
+            address(_core),
+            addresses.getAddress("TOKEN"),
+            addresses.getAddress("GAME_CONSUMER_PAYMENT_RECIPIENT"),
+            addresses.getAddress("WETH")
+        );
+
+        addresses.addAddress("GAME_CONSUMABLE", address(consumer));
+    }
 
     function _afterDeploy(Addresses addresses, address) internal override {}
 
