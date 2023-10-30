418bb348c75caeb7a1cdb8748627e2037c7fe924
LUKE™ <utx0@mettadata.com>
2023-10-30 12:36:06 +1100

    fix: added extra tests as per review.

--- test/integration/nfts/ERC1155AdminMinter.sol
+++ test/integration/nfts/ERC1155AdminMinter.sol
@@ -39,6 +39,46 @@ contract IntegrationTestERC1155AdminMinter is BaseTest {
         assertEq(erc1155Wearables.balanceOf(recipient, 108), 10_000);
     }
 
+    function testbulkMintTokensSuccess() public {
+        /// set supply cap
+        vm.startPrank(multisig);
+        erc1155Wearables.setSupplyCap(106, 10_000);
+        erc1155Wearables.setSupplyCap(108, 10_000);
+        assertEq(erc1155Wearables.maxTokenSupply(106), 10_000);
+        assertEq(erc1155Wearables.maxTokenSupply(108), 10_000);
+        vm.stopPrank();
+
+        ERC1155AdminMinter.BulkMint[] memory toMint = new ERC1155AdminMinter.BulkMint[](2);
+        toMint[0] = ERC1155AdminMinter.BulkMint(address(erc1155Wearables), 106, 10_000, recipient);
+        toMint[1] = ERC1155AdminMinter.BulkMint(address(erc1155Wearables), 108, 10_000, recipient);
+
+        /// bulkMintTokens
+        vm.prank(multisig);
+        erc1155AdminMinter.bulkMintTokens(toMint);
+
+        // assert
+        assertEq(erc1155Wearables.balanceOf(recipient, 106), 10_000);
+        assertEq(erc1155Wearables.balanceOf(recipient, 108), 10_000);
+    }
+
+    function testMintFailedNoSupply() public {
+        /// mint
+        vm.prank(multisig);
+        vm.expectRevert("BaseERC1155NFT: supply exceeded");
+        erc1155AdminMinter.mintToken(address(erc1155Wearables), recipient, 108, 10_000);
+    }
+
+    function testBulkMintTokensFailNoSupply() public {
+        ERC1155AdminMinter.BulkMint[] memory toMint = new ERC1155AdminMinter.BulkMint[](2);
+        toMint[0] = ERC1155AdminMinter.BulkMint(address(erc1155Wearables), 106, 10_000, recipient);
+        toMint[1] = ERC1155AdminMinter.BulkMint(address(erc1155Wearables), 108, 10_000, recipient);
+
+        /// bulkMintTokens
+        vm.prank(multisig);
+        vm.expectRevert("BaseERC1155NFT: supply exceeded");
+        erc1155AdminMinter.bulkMintTokens(toMint);
+    }
+
     function testMintFailOverSupplyLimit() public {
         /// mint
         vm.prank(multisig);
@@ -46,10 +86,47 @@ contract IntegrationTestERC1155AdminMinter is BaseTest {
         erc1155AdminMinter.mintToken(address(erc1155Wearables), recipient, 108, 100_000);
     }
 
+    function testBulkMintTokenFailOverSupplyLimit() public {
+        /// set supply cap
+        vm.startPrank(multisig);
+        erc1155Wearables.setSupplyCap(106, 10_000);
+        erc1155Wearables.setSupplyCap(108, 10_000);
+        assertEq(erc1155Wearables.maxTokenSupply(106), 10_000);
+        assertEq(erc1155Wearables.maxTokenSupply(108), 10_000);
+        vm.stopPrank();
+
+        ERC1155AdminMinter.BulkMint[] memory toMint = new ERC1155AdminMinter.BulkMint[](2);
+        toMint[0] = ERC1155AdminMinter.BulkMint(address(erc1155Wearables), 106, 20_000, recipient);
+        toMint[1] = ERC1155AdminMinter.BulkMint(address(erc1155Wearables), 108, 20_000, recipient);
+
+        /// bulkMintTokens
+        vm.prank(multisig);
+        vm.expectRevert("BaseERC1155NFT: supply exceeded");
+        erc1155AdminMinter.bulkMintTokens(toMint);
+    }
+
     function testMintFailNonAdminCaller() public {
         /// mint
         vm.expectRevert("CoreRef: no role on core");
         erc1155AdminMinter.mintToken(address(erc1155Wearables), recipient, 108, 10_000);
+    }
+
+    function testBulkMintTokenFailNonAdminCaller() public {
+        /// set supply cap
+        vm.startPrank(multisig);
+        erc1155Wearables.setSupplyCap(106, 10_000);
+        erc1155Wearables.setSupplyCap(108, 10_000);
+        assertEq(erc1155Wearables.maxTokenSupply(106), 10_000);
+        assertEq(erc1155Wearables.maxTokenSupply(108), 10_000);
+        vm.stopPrank();
+
+        ERC1155AdminMinter.BulkMint[] memory toMint = new ERC1155AdminMinter.BulkMint[](2);
+        toMint[0] = ERC1155AdminMinter.BulkMint(address(erc1155Wearables), 106, 20_000, recipient);
+        toMint[1] = ERC1155AdminMinter.BulkMint(address(erc1155Wearables), 108, 20_000, recipient);
+
+        /// bulkMintTokens
+        vm.expectRevert("CoreRef: no role on core");
+        erc1155AdminMinter.bulkMintTokens(toMint);
     }
 
     function testMintFailWhenPaused() public {
@@ -66,5 +143,28 @@ contract IntegrationTestERC1155AdminMinter is BaseTest {
         vm.prank(multisig);
         vm.expectRevert("Pausable: paused");
         erc1155AdminMinter.mintToken(address(erc1155Wearables), recipient, 108, 10_000);
+    }
+
+    function testBulkMintTokenFailWhenPaused() public {
+        /// set supply cap
+        vm.startPrank(multisig);
+        erc1155Wearables.setSupplyCap(106, 10_000);
+        erc1155Wearables.setSupplyCap(108, 10_000);
+        assertEq(erc1155Wearables.maxTokenSupply(106), 10_000);
+        assertEq(erc1155Wearables.maxTokenSupply(108), 10_000);
+        vm.stopPrank();
+
+        ERC1155AdminMinter.BulkMint[] memory toMint = new ERC1155AdminMinter.BulkMint[](2);
+        toMint[0] = ERC1155AdminMinter.BulkMint(address(erc1155Wearables), 106, 20_000, recipient);
+        toMint[1] = ERC1155AdminMinter.BulkMint(address(erc1155Wearables), 108, 20_000, recipient);
+
+        /// pause
+        vm.prank(multisig);
+        erc1155Wearables.pause();
+
+        /// bulkMintTokens
+        vm.prank(multisig);
+        vm.expectRevert("Pausable: paused");
+        erc1155AdminMinter.bulkMintTokens(toMint);
     }
 }
