e44dd2cc4e79de5a17e19d4651bbfd70ed1d5a81
intl-man <intlman@pm.me>
2024-04-18 13:57:45 +0700

    chore: updates as per feedback

--- script/deploy/BootstrapTestnet.s.sol
+++ script/deploy/BootstrapTestnet.s.sol
@@ -17,6 +17,7 @@ import {zip007} from "@proposals/zips/zip007.sol";
 import {zip008} from "@proposals/zips/zip008.sol";
 import {zip009} from "@proposals/zips/zip009.sol";
 import {zip010} from "@proposals/zips/zip010.sol";
+import {zip011} from "@proposals/zips/zip010.sol";
 
 /*
 How to use:
@@ -54,6 +55,7 @@ contract BootstrapTestnet is Script {
         proposals.push(Proposal(address(new zip008()))); /// MaxSupply settings proposal
         proposals.push(Proposal(address(new zip009()))); /// MaxSupply settings proposal
         proposals.push(Proposal(address(new zip010()))); /// MaxSupply settings proposal
+        proposals.push(Proposal(address(new zip011()))); /// MaxSupply settings proposal
     }
 
     function run() public {
