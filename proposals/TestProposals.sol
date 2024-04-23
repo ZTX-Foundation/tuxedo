f29d0d992d1dc5a2db5831fd73086ac5987452d1
prateek <prateekgupta105@gmail.com>
2024-04-23 21:03:38 +0530

    Merge branch 'develop' into feat/proposal-cleanup

--- proposals/TestProposals.sol
+++ proposals/TestProposals.sol
@@ -20,6 +20,7 @@ import {zip008} from "@proposals/zips/zip008.sol";
 import {zip009} from "@proposals/zips/zip009.sol";
 import {zip010} from "@proposals/zips/zip010.sol";
 import {zip011} from "@proposals/zips/zip011.sol";
+import {zip012} from "@proposals/zips/zip012.sol";
 import {zipTest} from "@proposals/zips/zipTest.sol";
 
 /*
@@ -54,6 +55,7 @@ contract TestProposals is Test {
             proposals.push(Proposal(address(new zip009()))); /// MaxSupply settings proposal
             proposals.push(Proposal(address(new zip010()))); /// MaxSupply settings proposal
             proposals.push(Proposal(address(new zip011()))); /// MaxSupply settings proposal
+            proposals.push(Proposal(address(new zip012()))); /// MaxSupply settings proposal
         }
 
         proposals.push(Proposal(address(new zipTest()))); /// RnD/testing only proposal
