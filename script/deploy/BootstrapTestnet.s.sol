4f6413531a54521109f1e0014da3f5df3cc814eb
intl-man <intlman@pm.me>
2024-05-06 12:39:54 +0800

    chore: wearables max supply updates

--- script/deploy/BootstrapTestnet.s.sol
+++ script/deploy/BootstrapTestnet.s.sol
@@ -21,6 +21,7 @@ import {zip011} from "@proposals/zips/zip011.sol";
 import {zip012} from "@proposals/zips/zip012.sol";
 import {zip013} from "@proposals/zips/zip013.sol";
 import {zip014} from "@proposals/zips/zip014.sol";
+import {zip016} from "@proposals/zips/zip016.sol";
 
 /*
 How to use:
@@ -60,6 +61,7 @@ contract BootstrapTestnet is Script {
         proposals.push(Proposal(address(new zip012()))); /// MaxSupply settings proposal
         proposals.push(Proposal(address(new zip013()))); /// MaxSupply settings proposal
         proposals.push(Proposal(address(new zip014()))); /// MaxSupply settings proposal
+        proposals.push(Proposal(address(new zip016()))); /// MaxSupply settings proposal
     }
 
     function run() public {
