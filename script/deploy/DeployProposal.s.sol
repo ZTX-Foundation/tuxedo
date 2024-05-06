8ac4f91cbd3d1d74f01bb99aa3774fe96592f660
_IM <70502803+intl-man@users.noreply.github.com>
2024-05-06 16:02:20 -0700

    chore: wearables max supply updates (#234)

--- script/deploy/DeployProposal.s.sol
+++ script/deploy/DeployProposal.s.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.8.18;
 
 import {console} from "@forge-std/console.sol";
-import {zip014 as zip} from "@proposals/zips/zip014.sol";
+import {zip016 as zip} from "@proposals/zips/zip016.sol";
 import {Script} from "@forge-std/Script.sol";
 import {Addresses} from "@proposals/Addresses.sol";
 import {TimelockProposal} from "@proposals/proposalTypes/TimelockProposal.sol";
