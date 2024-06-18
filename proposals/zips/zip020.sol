d9675b170097162f9b7306c651568a1acf04cf05
intl-man <intlman@pm.me>
2024-06-18 17:42:36 +0800

    chore: max supply updates

---
+++ proposals/zips/zip020.sol
@@ -0,0 +1,150 @@
+//SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.18;
+
+import {TimelockProposal} from "@forge-proposal-simulator/src/proposals/TimelockProposal.sol";
+
+import {ERC1155MaxSupplyMintable} from "@protocol/nfts/ERC1155MaxSupplyMintable.sol";
+
+contract zip020 is TimelockProposal {
+
+    struct TokenIDMaxSupplySettings {
+        uint256 maxSupply;
+        uint256 tokenId;
+    }
+
+    struct Collections {
+        TokenIDMaxSupplySettings[] placeables;
+        TokenIDMaxSupplySettings[] wearables;
+    }
+
+    TokenIDMaxSupplySettings[] private placeableTokenIDMaxSupplySettings;
+    TokenIDMaxSupplySettings[] private wearableTokenIDMaxSupplySettings;
+
+    /// @notice ERC1155 collections
+    ERC1155MaxSupplyMintable placeable;
+    ERC1155MaxSupplyMintable wearable;
+
+    // Returns the name of the proposal.
+    function name() public pure override returns (string memory) {
+        return "ZIP020";
+    }
+
+    // Provides a brief description of the proposal.
+    function description() public pure override returns (string memory) {
+        return "ZTX CGv1.4 MaxSupply updates";
+    }
+
+    function _setAndConfirmData() private {
+        // Wearable and placeable data
+        string memory data = string(
+            abi.encodePacked(vm.readFile("./proposals/zips/zip020.json"))
+        );
+
+        bytes memory parsedJson = vm.parseJson(data);
+
+        Collections memory decodedData = abi.decode(
+            parsedJson,
+            (Collections)
+        );
+
+        for (uint256 i = 0; i < decodedData.placeables.length; i++) {
+            placeableTokenIDMaxSupplySettings.push(
+                TokenIDMaxSupplySettings(
+                    decodedData.placeables[i].maxSupply,
+                    decodedData.placeables[i].tokenId
+                )
+            );
+        }
+
+        for (uint256 i = 0; i < decodedData.wearables.length; i++) {
+            wearableTokenIDMaxSupplySettings.push(
+                TokenIDMaxSupplySettings(
+                    decodedData.wearables[i].maxSupply,
+                    decodedData.wearables[i].tokenId
+                )
+            );
+        }
+
+        /// @notice sanity checks for placeables
+        assertEq(placeableTokenIDMaxSupplySettings.length, 155, "Invalid placeableTokenIDMaxSupplySettings length");
+
+        uint placeableMaxSupplyTotal = 0;
+
+        for (uint256 i = 0; i < placeableTokenIDMaxSupplySettings.length; i++) {
+            placeableMaxSupplyTotal += placeableTokenIDMaxSupplySettings[i].maxSupply;
+        }
+
+        assertEq(placeableMaxSupplyTotal, 15520000, "Invalid maxSupplyTotal for placeables");
+
+        /// @notice sanity checks for wearables
+        assertEq(wearableTokenIDMaxSupplySettings.length, 23, "Invalid wearableTokenIDMaxSupplySettings length");
+
+        uint wearableMaxSupplyTotal = 0;
+
+        for (uint256 i = 0; i < wearableTokenIDMaxSupplySettings.length; i++) {
+            wearableMaxSupplyTotal += wearableTokenIDMaxSupplySettings[i].maxSupply;
+        }
+
+        assertEq(wearableMaxSupplyTotal, 4512200, "Invalid maxSupplyTotal for wearables");
+    }
+
+    function build()
+        public
+        override
+        buildModifier(addresses.getAddress("ADMIN_TIMELOCK_CONTROLLER")) 
+    {
+         /// @notice placeable config
+        for (uint256 i = 0; i < placeableTokenIDMaxSupplySettings.length; i++) {
+            placeable.setSupplyCap(placeableTokenIDMaxSupplySettings[i].tokenId, placeableTokenIDMaxSupplySettings[i].maxSupply);
+        }
+
+        /// @notice wearable config
+        for (uint256 i = 0; i < wearableTokenIDMaxSupplySettings.length; i++) {
+            wearable.setSupplyCap(wearableTokenIDMaxSupplySettings[i].tokenId, wearableTokenIDMaxSupplySettings[i].maxSupply);
+        }
+    }
+
+    function run() public override {
+        setTimelock(addresses.getAddress("ADMIN_TIMELOCK_CONTROLLER"));
+
+        placeable = ERC1155MaxSupplyMintable(
+            addresses.getAddress("ERC1155_MAX_SUPPLY_MINTABLE_PLACEABLES")
+        );
+        wearable = ERC1155MaxSupplyMintable(
+            addresses.getAddress("ERC1155_MAX_SUPPLY_MINTABLE_WEARABLES")
+        );
+
+        _setAndConfirmData();
+
+        super.run();
+    }
+
+    function simulate() public override {
+        address multisig = addresses.getAddress("ADMIN_MULTISIG");
+
+        /// Multisig is proposer and executor
+        _simulateActions(multisig, multisig);
+    }
+
+    function validate() public override {
+        /// @notice verify placeables
+        for (uint256 i = 0; i < placeableTokenIDMaxSupplySettings.length; i++) {
+            uint256 tokenId = placeableTokenIDMaxSupplySettings[i].tokenId;
+            uint256 maxSupply = placeableTokenIDMaxSupplySettings[i].maxSupply;
+            uint256 currentSupply = placeable.totalSupply(tokenId);
+
+            assertEq(placeable.maxTokenSupply(tokenId), maxSupply, "Invalid maxTokenSupply for tokenId");
+            assertEq(placeable.getMintAmountLeft(tokenId), maxSupply - currentSupply, "Invalid getMintAmountLeft for tokenId");
+        }
+
+        /// @notice verify wearables
+        for (uint256 i = 0; i < wearableTokenIDMaxSupplySettings.length; i++) {
+            uint256 tokenId = wearableTokenIDMaxSupplySettings[i].tokenId;
+            uint256 maxSupply = wearableTokenIDMaxSupplySettings[i].maxSupply;
+            uint256 currentSupply = wearable.totalSupply(tokenId);
+
+            assertEq(wearable.maxTokenSupply(tokenId), maxSupply, "Invalid maxTokenSupply for tokenId");
+            assertEq(wearable.getMintAmountLeft(tokenId), maxSupply - currentSupply, "Invalid getMintAmountLeft for tokenId");
+        }
+    }
+}
--- script/deploy/DeployProposal.s.sol
+++ script/deploy/DeployProposal.s.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.18;
 
 import {console} from "@forge-std/console.sol";
-import {zip019 as zip} from "proposals/zips/zip019.sol";
+import {zip020 as zip} from "proposals/zips/zip020.sol";
 import {Script} from "@forge-std/Script.sol";
 import {Addresses} from "@forge-proposal-simulator/addresses/Addresses.sol";
 import {TimelockProposal} from "@forge-proposal-simulator/src/proposals/TimelockProposal.sol";
